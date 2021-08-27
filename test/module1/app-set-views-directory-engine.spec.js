aeabf7e39a8dce233eea46f05ae8fb4b2291a36d
Sergey Afonchenko <Sergei.Afonchenko@emergn.com>
2021-08-27 22:51:13 +0300

    Module 1

--- test/module1/app-set-views-directory-engine.spec.js
+++ test/module1/app-set-views-directory-engine.spec.js
@@ -4,6 +4,7 @@ describe('View engine and directory', () => {
   it('should set view engine and directory @app-set-views-directory-engine', () => {
     assert(typeof app === 'function', '`app` const has not been created in `app.js`.');
     assert(app.settings.views === path.join(__dirname, '../../src/views'), 'The view directory has not been set to the `views` directory.');
+    console.log(path.join(__dirname, '../../src/views'))
     assert(app.settings['view engine'] === 'ejs', 'The view engine has not been set to `ejs`.');
   });
 });
